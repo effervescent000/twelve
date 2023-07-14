13953a5c8d1159eaa90e701d7dcc507fce4d41ce
effervescent000 <kislio01@gmail.com>
2023-07-14 16:47:16 -0400

    simple spell damage

--- src/components/damage-pane.svelte
+++ src/components/damage-pane.svelte
@@ -2,17 +2,14 @@
 	import { page } from '$app/stores';
 	import { getSpeedMod } from '../constants/characters';
 	import { WEAPONS, WEAPON_TYPES } from '../constants/gear';
+	import { SPELLS, spellDamageFormula } from '../constants/spells';
 	import { characterStore, statsStore } from '../stores';
 
 	// PROPS
 
 	// STATE
-	let attackDamage = 0;
-	// currently I am not taking into account action time
-	// because I am having a hard time figuring out how it's determined
-	// is it a set time for everything? does it depend on your weapon?
-	// all I can find is that it can't be modified
-	let chargeTime = 0;
+	let attackDPS = 0;
+	let spellDPS = 0;
 
 	// LOGIC
 	const targetDefense = 6;
@@ -25,31 +22,41 @@
 		if (weapon) {
 			const damageFormula =
 				WEAPONS[weapon].damageFormula || WEAPON_TYPES[WEAPONS[weapon].type].damageFormula;
-			attackDamage = damageFormula({
-				attackPower: WEAPONS[weapon].attack,
-				characterStats,
-				characterData,
-				targetDefense
-			});
+			attackDPS =
+				damageFormula({
+					attackPower: WEAPONS[weapon].attack,
+					characterStats,
+					characterData,
+					targetDefense
+				}) /
+				(((WEAPONS[weapon].ct || WEAPON_TYPES[WEAPONS[weapon].type].ct) *
+					getSpeedMod(characterStats.speed) +
+					0.25) *
+					1 *
+					0.5 +
+					1.2);
 		}
 	}
 
 	$: {
-		const weapon = characterData.weapon;
-		if (weapon) {
-			// the `1` in the below calculation is for swiftness licenses which
-			// I'm just not dealing with yet
-			chargeTime =
-				((WEAPONS[weapon].ct || WEAPON_TYPES[WEAPONS[weapon].type].ct) *
-					getSpeedMod(characterStats.speed) +
-					0.25) *
-				1 *
-				0.5;
+		const spell = characterData.spell;
+		if (spell) {
+			const damage = spellDamageFormula({
+				power: SPELLS[spell].power,
+				characterData,
+				characterStats,
+				targetDefense
+			});
+			spellDPS = damage / ((23 * getSpeedMod(characterStats.speed) + 0.25) * 1 * 0.5 + 1.2);
 		}
 	}
 </script>
 
 <div>
-	Attack damage: {Math.round(attackDamage)}
-	Attack DPS: {Math.round(attackDamage / (chargeTime + 1.2))}
+	<div>
+		Attack DPS: {Math.round(attackDPS)}
+	</div>
+	<div>
+		Spell DPS: {Math.round(spellDPS)}
+	</div>
 </div>
